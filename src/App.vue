35fff0522c03d75acf64aae2dea9e6be33aa472f
haishenming <haishenmingx@gmail.com>
2021-08-08 10:50:28 +0800

    时间戳功能

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,47 @@
 <template>
-  <img src="./assets/logo.png">
-  <div>
-  <Timestamp msg="Welcome to Your Vue.js App"/>
-  </div>
+     <el-container>
+    <el-header>
+      <el-menu
+        :default-active="activeIndex"
+        class="el-menu-demo"
+        mode="horizontal"
+        @select="handleSelect"
+      >
+        <el-menu-item index="1"><router-link to="/">首页</router-link></el-menu-item>
+        <el-menu-item index="2"><router-link to="/Xbox">XBox</router-link></el-menu-item>
+          <!-- <el-menu-item index="2-2">选项2</el-menu-item> -->
+          <!-- <el-menu-item index="2-3">选项3</el-menu-item> -->
+          <!-- <el-submenu index="2-4">
+      <template #title>选项4</template>
+      <el-menu-item index="2-4-1">选项1</el-menu-item>
+      <el-menu-item index="2-4-2">选项2</el-menu-item>
+      <el-menu-item index="2-4-3">选项3</el-menu-item>
+    </el-submenu> -->
+        <!-- <el-menu-item index="3" disabled>消息中心</el-menu-item>
+        <el-menu-item index="4"
+          ><a href="https://www.ele.me" target="_blank"
+            >订单管理</a
+          ></el-menu-item
+        > -->
+      </el-menu>
+    </el-header>
+
+    <el-main>
+      <router-view></router-view>
+    </el-main>
+
+    <el-footer></el-footer>
+  </el-container>
 </template>
 
 <script>
-import Timestamp from './components/Timestamp.vue'
 
 export default {
   name: 'App',
   components: {
-    Timestamp
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
 import ElementPlus from 'element-plus'
 import 'element-plus/lib/theme-chalk/index.css'
 import installElementPlus from './plugins/element'
 
 const app = createApp(App)
 installElementPlus(app)
-app.use(ElementPlus).mount('#app')
+app.use(ElementPlus).use(router).mount('#app')
